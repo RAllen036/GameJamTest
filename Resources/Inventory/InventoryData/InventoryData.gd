9a56e4111bd453dac2a9ff902532e0e8ee8a01a9
RAllen036 <ryanallen783@gmail.com>
2024-07-03 15:58:53 +0100

    Inventory Data organisation via resources
    Resource scripts for items, slots and inventories

---
+++ Resources/Inventory/InventoryData/InventoryData.gd
@@ -0,0 +1,5 @@
+class_name InventoryData
+extends Resource
+
+@export_category("Slots")
+@export var data: Array[SlotData]
---
+++ Resources/Inventory/Items/ItemData.gd
@@ -0,0 +1,7 @@
+class_name ItemData
+extends Resource
+
+@export_placeholder("Enter Item Name") var name: String
+@export_multiline var description: String
+@export var stackable: bool = true
+@export var icon: AtlasTexture
