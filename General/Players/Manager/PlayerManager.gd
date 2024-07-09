8dac973bc544af6d58e4bbd1ce12f001037ac967
RAllen036 <ryanallen783@gmail.com>
2024-07-09 22:52:18 +0100

    Connected the temp main world as the main world

--- General/Players/Manager/PlayerManager.gd
+++ General/Players/Manager/PlayerManager.gd
@@ -3,6 +3,17 @@ extends Node
 
 var player: CharacterBody2D
 
+func create_player() -> CharacterBody2D:
+	return null
+
+func create_new_player_inventory(inv_data: InventoryData = null) -> InventoryData:
+	if not inv_data:
+		var new_inv_data: InventoryData = InventoryData.new()
+		for i in range(0, 7): new_inv_data.data.append(SlotData.new())
+		return new_inv_data
+	
+	return inv_data
+
 func use_slot_data(slot_data: SlotData) -> void:
 	slot_data.item_data.use(player)
 
